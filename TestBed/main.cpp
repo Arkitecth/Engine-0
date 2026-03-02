ff1059b9cc2acb5e4d8d51e79357b98cdce3f6cb
Trent_T <trenttuedon2000@gmail.com>
2026-03-02 10:52:46 -0500

    Reorgainzed Directory Structure

---
+++ TestBed/main.cpp
@@ -0,0 +1,27 @@
+#include <Color.h>
+#include <Vector.h>
+#include <Engine.h>
+#include <Rectangle.h>
+#include <utility.h>
+#include <iostream>
+
+void TestRectangle()
+{
+	E0::Rectangle rect{E0::Vector{100, 100}, 100, 100, E0::RED}; 
+
+	E0::Rectangle rectB{E0::Vector{100, 100}, 100, 100, E0::RED}; 
+
+
+	if (CheckForIntersection(rect, rectB)) 
+	{
+		std::cout << "Test Passed" << '\n';
+	} else {
+		std::cout << "Test Failed" << '\n';
+	}
+
+}
+
+int main()
+{
+	TestRectangle();
+}
