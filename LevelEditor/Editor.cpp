c301581df898f0866a160613a6ffe406642ab86e
Trent_T <trenttuedon2000@gmail.com>
2026-03-19 09:48:05 -0400

    Add Load Level Button

--- LevelEditor/Editor.cpp
+++ LevelEditor/Editor.cpp
@@ -4,8 +4,10 @@
 #include "imgui.h"
 #include "imgui_impl_sdl3.h"
 #include "imgui_impl_sdlrenderer3.h"
+#include <algorithm>
 #include <iostream>
-
+#include <fstream>
+#include <string>
 
 Editor::Editor(int new_width, int new_height)
 {
@@ -36,6 +38,7 @@ Editor::~Editor()
 
 void SDLCALL Editor::createLevel(void* userdata, const char* const* fileList, int filter)
 {
+	std::fstream levelFile{};
 	if (!fileList) 
 	{
 		SDL_Log("An error occurred: %s\n", SDL_GetError()); 
@@ -49,9 +52,57 @@ void SDLCALL Editor::createLevel(void* userdata, const char* const* fileList, in
 	{
 		E0::Level* currentLevel = reinterpret_cast<E0::Level*>(userdata); 
 		currentLevel->setTexturePath(*fileList); 
-		SDL_Log("Level was succesfully created"); 
+		levelFile.open("./Levels/" + currentLevel->getLevelsName() + ".lvl", std::ios::out | std::ios::in);
+		if (levelFile.is_open()) { levelFile << "- Name:" << currentLevel->getLevelsName() << '\n';
+			levelFile << "- Texture Path:" << currentLevel->getTexturePath() << '\n';
+			SDL_Log("Level was succesfully created"); 
+			levelFile.close();
+		}
 	}
+	if (filter < 0) {
+		SDL_Log("The current platform does not support fetching the selected filter, or the user did not select any filter");
+	} else if(filter < SDL_arraysize(filters)) {
+		SDL_Log("The filter selected by the user is '%s' (%s).", 
+			filters[filter].pattern, filters[filter].name);
+	}
+}
 
+void SDLCALL Editor::loadLevel(void* userdata, const char* const* fileList, int filter)
+{
+	std::fstream levelFile{};
+	std::string line{};
+	std::string levelName; 
+	std::string nameTag = "- Name:"; 
+	std::string textureTag = "- Texture Path:"; 
+	if (!fileList) 
+	{
+		SDL_Log("An error occurred: %s\n", SDL_GetError()); 
+		return;
+	} else if(!*fileList) {
+		SDL_Log("the user did not select any file"); 
+		SDL_Log("Most likely, the dialog was canceled."); 
+		return;
+	}
+	if (*fileList) 
+	{
+		E0::Level* currentLevel = reinterpret_cast<E0::Level*>(userdata); 
+		levelFile.open(*fileList, std::ios::in);
+		if (levelFile.is_open()) {
+			while (std::getline(levelFile, line)) {
+				std::size_t namePos= line.find(nameTag); 
+				if (namePos != std::string::npos) 
+				{
+					currentLevel->setLevelsName(line.substr(nameTag.size()));
+				}
+				std::size_t texturePos = line.find(textureTag); 
+				if (texturePos != std::string::npos) 
+				{
+					currentLevel->setTexturePath(line.substr(textureTag.size()));
+					std::cout << currentLevel->getTexturePath() << '\n';
+				}
+			}
+		}
+	}
 	if (filter < 0) {
 		SDL_Log("The current platform does not support fetching the selected filter, or the user did not select any filter");
 	} else if(filter < SDL_arraysize(filters)) {
@@ -62,8 +113,11 @@ void SDLCALL Editor::createLevel(void* userdata, const char* const* fileList, in
 
 
 
+
+
 void Editor::run()
 {
+
 	isRunning = true;
 	while (isRunning) 
 	{
@@ -89,32 +143,38 @@ void Editor::run()
 		ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), DM.getRenderer());
 		SDL_RenderPresent(DM.getRenderer());
 
-
 	
 	}
 }
 
-
 void Editor::drawLayout()
 {
-
 	if(ImGui::Button("Create Level"))
 	{
 		ImGui::OpenPopup("LevelEntry"); 
 	}
+	ImGui::SameLine(120.0f);
+	if (ImGui::Button("Load Level")) 
+	{
+		SDL_ShowOpenFileDialog(loadLevel, &currentLevel, DM.getWindow(), levelFilter, 1, "./Levels/", false); 
+		currentLevel.setLevelsName(inputBuffer);
+		std::fill(inputBuffer, inputBuffer + sizeof(inputBuffer), '\0'); 
+	}
+
 	if (ImGui::BeginPopup("LevelEntry")) 
 	{
-		char buf[100]{};
-		ImGui::InputText("Levels Name", buf, 100);
+		ImGui::InputText("Levels Name", inputBuffer, 100);
 		if(ImGui::Button("Create"))
 		{
+			currentLevel.setLevelsName(inputBuffer);
 			SDL_ShowOpenFileDialog(createLevel, &currentLevel, DM.getWindow(), filters, 1, "./", false); 
-			currentLevel.setLevelsName(buf);
+			std::fill(inputBuffer, inputBuffer + sizeof(inputBuffer), '\0'); 
 			ImGui::CloseCurrentPopup();
 		}
 		ImGui::SameLine(75.0f);
 		if(ImGui::Button("Cancel"))
 		{
+			std::fill(inputBuffer, inputBuffer + sizeof(inputBuffer), '\0'); 
 			ImGui::CloseCurrentPopup();
 		}
 		ImGui::EndPopup();
@@ -124,6 +184,7 @@ void Editor::drawLayout()
 		ImGui::Text("%s", currentLevel.getLevelsName().c_str());
 	}
 }
+
 
 
 
--- LevelEditor/Editor.h
+++ LevelEditor/Editor.h
@@ -1,16 +1,21 @@
 #include "Level.h"
+#include "SDL3/SDL_dialog.h"
 #include "imgui.h"
-
-static const SDL_DialogFileFilter filters[] = 
+const SDL_DialogFileFilter filters[] = 
 {
 		{"PNG Images ", "png"},  
 		{"JPEG Images ", "jpg;jpeg"},  
 }; 
 
+const SDL_DialogFileFilter levelFilter[] = {
+    {"Engine 0 Level Files", "lvl"}
+};
+
 class Editor 
 {
     private:
             E0::Level currentLevel{};
+            char inputBuffer[100]{};
             int width{};
             int height{};
             bool isRunning{};
@@ -26,4 +31,7 @@ class Editor
         //void SDLCALL createLevel(void* userdata, const char* const* fileList, int filter); 
         void run(); 
     static void SDLCALL createLevel(void* userdata, const char* const* fileList, int filter); 
+    static void SDLCALL loadLevel(void* userdata, const char* const* fileList, int filter); 
+
+    void exportFile(E0::Level& level);
 }; 
